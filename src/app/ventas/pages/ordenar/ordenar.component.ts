b0b09077efad4eb4a1e161365b5b6e122bb30f44
e_iroig <ignacio.roig@capgemini.com>
2023-03-06 11:21:57 +0100

    04-pipesApp Pipes a medida

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,5 +8,44 @@ import { Component } from '@angular/core';
   ]
 })
 export class OrdenarComponent {
+  enMayusculas: boolean = true;
 
+  ordenarPor: string = '';
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
+  cambiarEnMayusculas () {
+    this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden ( valor: string ) {
+    this.ordenarPor = valor;
+    console.log(valor);
+  }
 }
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -8,15 +8,23 @@ import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
-
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 @NgModule({
   declarations: [
+    //Components
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
-    OrdenarComponent
+    OrdenarComponent,
+
+    //Pipes
+    MayusculasPipe,
+    VuelaPipe,
+    OrdenarPipe
   ],
   exports: [
     NumerosComponent,
